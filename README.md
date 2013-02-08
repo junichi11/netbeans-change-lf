2a3d345ae825bdf7aa158e27ae95566be7335b6e
junichi11 <junichi0206@gmail.com>
2013-02-08 16:50:04 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Change Line Feed Code Plugin
+Change line feed code plugin for NetBeans 7.3+
+
+## Feature
+This plugin changes line feed code of files to specified lf code.
+
+## Options
+
+Tools > Options > Editor > Change LF
+
+- enable
+- line feed code kind : LF, CRLF, CR
+- show confirm dialog before change line feed
+
+If you check "enable", this feature will be enabled. Then, please choose LF kind.
+If you check the third option and lf code of file is not specified lf code,
+confirmation dialog is displayed.
+
+## License
+[Common Development and Distribution License (CDDL) v1.0 and GNU General Public License (GPL) v2](http://netbeans.org/cddl-gplv2.html)
---
+++ src/com/junichi11/netbeans/changelf/ui/actions/ToggleEnableChangeLFAction.java
@@ -0,0 +1,99 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright 2013 Oracle and/or its affiliates. All rights reserved.
+ *
+ * Oracle and Java are registered trademarks of Oracle and/or its affiliates.
+ * Other names may be trademarks of their respective owners.
+ *
+ * The contents of this file are subject to the terms of either the GNU
+ * General Public License Version 2 only ("GPL") or the Common
+ * Development and Distribution License("CDDL") (collectively, the
+ * "License"). You may not use this file except in compliance with the
+ * License. You can obtain a copy of the License at
+ * http://www.netbeans.org/cddl-gplv2.html
+ * or nbbuild/licenses/CDDL-GPL-2-CP. See the License for the
+ * specific language governing permissions and limitations under the
+ * License.  When distributing the software, include this License Header
+ * Notice in each file and include the License file at
+ * nbbuild/licenses/CDDL-GPL-2-CP.  Oracle designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Oracle in the GPL Version 2 section of the License file that
+ * accompanied this code. If applicable, add the following below the
+ * License Header, with the fields enclosed by brackets [] replaced by
+ * your own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ *
+ * If you wish your version of this file to be governed by only the CDDL
+ * or only the GPL Version 2, indicate your decision by adding
+ * "[Contributor] elects to include this software in this distribution
+ * under the [CDDL or GPL Version 2] license." If you do not indicate a
+ * single choice of license, a recipient has the option to distribute
+ * your version of this file under either the CDDL, the GPL Version 2 or
+ * to extend the choice of license to its licensees as provided above.
+ * However, if you add GPL Version 2 code and therefore, elected the GPL
+ * Version 2 license, then the option applies only if the new code is
+ * made subject to such option by the copyright holder.
+ *
+ * Contributor(s):
+ *
+ * Portions Copyrighted 2013 Sun Microsystems, Inc.
+ */
+package com.junichi11.netbeans.changelf.ui.actions;
+
+import com.junichi11.netbeans.changelf.ui.options.ChangeLFOptions;
+import java.awt.event.ActionEvent;
+import org.openide.awt.ActionID;
+import org.openide.awt.ActionReference;
+import org.openide.awt.ActionReferences;
+import org.openide.awt.ActionRegistration;
+import org.openide.util.HelpCtx;
+import org.openide.util.NbBundle.Messages;
+import org.openide.util.actions.BooleanStateAction;
+
+@ActionID(
+        category = "File",
+        id = "com.junichi11.netbeans.changelf.ui.actions.ToggleEnableChangeLFAction")
+@ActionRegistration(
+        lazy = false,
+        displayName = "#CTL_ToggleEnableChangeLFAction")
+@ActionReferences({
+    @ActionReference(path = "Toolbars/File", position = 500),
+    @ActionReference(path = "Shortcuts", name = "DS-L")
+})
+@Messages("CTL_ToggleEnableChangeLFAction=Toggle enable change LF")
+public final class ToggleEnableChangeLFAction extends BooleanStateAction{
+    private static final long serialVersionUID = -538737826355249808L;
+    private static final String LF_ICON_16 = "com/junichi11/netbeans/changelf/ui/actions/lf_16.png"; // NOI18N
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        ChangeLFOptions options = ChangeLFOptions.getInstance();
+        boolean enable = options.isEnable();
+        options.setEnable(!enable);
+        setBooleanState(!enable);
+    }
+
+    @Override
+    public String getName() {
+        return Bundle.CTL_ToggleEnableChangeLFAction();
+    }
+
+    @Override
+    public HelpCtx getHelpCtx() {
+        return HelpCtx.DEFAULT_HELP;
+    }
+
+    @Override
+    protected String iconResource() {
+        return LF_ICON_16;
+    }
+
+    @Override
+    protected void initialize() {
+        super.initialize();
+        ChangeLFOptions options = ChangeLFOptions.getInstance();
+        setBooleanState(options.isEnable());
+    }
+    
+}
